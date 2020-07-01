28f8014f6933a704956d2de41a7ced1c4262c0aa
VAnhTrinh4869 <trinhvanh4869@gmail.com>
2020-07-01 09:03:45 +0700

    change config db

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('pet_dating_test', 'root', 'Passmysql4869', {
-    host: 'localhost',
+//mysql://bf8a1b55c1e2e6:8c8d6ed8@us-cdbr-east-02.cleardb.com/heroku_0d15c6611609f1f?reconnect=true
+const sequelize = new Sequelize('heroku_0d15c6611609f1f', 'bf8a1b55c1e2e6', '8c8d6ed8', {
+    host: 'us-cdbr-east-02.cleardb.com',
     dialect: 'mysql'
 });
 
