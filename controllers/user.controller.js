5219b2ce64405ce8c81632899757932ad7c814be
VAnhTrinh4869 <trinhvanh4869@gmail.com>
2020-07-09 15:51:58 +0700

    update insert_user api, create get user by id api

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,23 +7,60 @@ module.exports.getAll = (req, res) => {
         .catch(error => console.log(error));
 }
 
-module.exports.insertNewUser = (req, res) => {
-    let sql = 'INSERT INTO user (name, email, access_token) VALUES (:name, :email, :access_token)';
-    db.query(sql, { replacements: { ...req.body } })
-        .then(result => {
-            res.json({
-                result: 'ok',
-                data: {
-                    id: result[0],
-                    ...req.body
-                },
-                message: 'Insert new user successfully!'
+module.exports.get = (req, res) => {
+    let sql = 'SELECT * FROM user WHERE user_id = :id';
+    db.query(sql, { replacements: { id: req.params.id }, type: db.QueryTypes.SELECT })
+        .then(user => {
+            res.json(user)
+        })
+        .catch(error => console.log(error));
+}
+
+const buildInsertSql = (body, tbl) => {
+    let sql = `INSERT INTO ${tbl} (`;
+    let values = ` VALUES (`;
+    for (let property in body) {
+        sql += `${property},`;
+        values += `:${property},`;
+    }
+    return sql.slice(0, -1) + ')' + values.slice(0, -1) + ')';
+}
+module.exports.insertNewUser = async (req, res) => {
+    let beforeInsert = 'SELECT COUNT(1) as count FROM user WHERE token = :token';
+    let isLogin;
+    let result = await db.query(beforeInsert, { replacements: { token: req.body.token }, type: db.QueryTypes.SELECT });
+    isLogin = result[0].count > 0;
+    if (!isLogin) {
+        let sql = buildInsertSql(req.body, 'user');
+        db.query(sql, { replacements: { ...req.body } })
+            .then(result => {
+                res.json({
+                    result: 'ok',
+                    data: {
+                        user_id: result[0],
+                        ...req.body
+                    },
+                    message: 'Insert new user successfully!'
+                })
+            }).catch(error => {
+                res.json({
+                    result: 'failed',
+                    data: {},
+                    message: `Error is ${error}`
+                })
+            });
+    } else {
+        let sql = 'SELECT * FROM user WHERE token = :token';
+        db.query(sql, { replacements: { token: req.body.token }, type: db.QueryTypes.SELECT })
+            .then(user => {
+                res.json({
+                    result: 'ok',
+                    data: {
+                        ...user[0]
+                    },
+                    message: 'Insert new user successfully!'
+                })
             })
-        }).catch(error => {
-            res.json({
-                result: 'failed',
-                data: {},
-                message: `Error is ${error}`
-            })
-        });
+            .catch(error => console.log(error));
+    }
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const controller = require('../controllers/user.controller');
 
-router.get('/', (req, res)=>res.send('Hello'));
+router.get('/', (req, res) => res.send('Hello'));
 
 router.get('/getAll', controller.getAll);
+
+router.get('/:id', controller.get);
 
 router.post('/insert_new_user', controller.insertNewUser);
 
