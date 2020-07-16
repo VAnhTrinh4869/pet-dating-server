522b46d2aea278e4b6fa63997aeab102ae1aad27
VAnhTrinh4869 <trinhvanh4869@gmail.com>
2020-07-16 23:23:43 +0700

    revert

---
+++ controllers/api.controller.js
@@ -0,0 +1,16 @@
+const db = require('../db');
+const common = require('../common');
+
+module.exports.getProfile = (req, res) => {
+    let sql = `SELECT u.name, u.email, COUNT(DISTINCT(p.id)) AS pets, COUNT(pm.pet_id1) AS matches 
+                FROM user u
+                LEFT JOIN pet p ON u.id = p.user_id
+                LEFT JOIN pet_match pm ON p.id = pm.pet_id1
+                WHERE u.id = :id
+                GROUP BY u.id `;
+    db.query(sql, { replacements: { id: req.userId }, type: db.QueryTypes.SELECT })
+        .then(results => {
+            res.json(results)
+        })
+        .catch(error => res.json({ error: error }));
+}
