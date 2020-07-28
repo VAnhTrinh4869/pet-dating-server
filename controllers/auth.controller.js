e00cc9b10d2b86397051a571ca985a3a6e430152
VAnhTrinh4869 <trinhvanh4869@gmail.com>
2020-07-28 20:57:10 +0700

    change token from id => uid

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,12 +4,12 @@ const db = require('../db');
 const common = require('../common');
 
 module.exports.insertNewUser = async (req, res) => {
-    if (!req.body.email) {
-        res.send(res.status(422).send('you must be logged in'));
-    }
+    // if (!req.body.email) {
+    //     res.send(res.status(422).send('you must be logged in'));
+    // }
 
-    let beforeInsert = 'SELECT COUNT(1) as count FROM user WHERE email = :email';
-    let result = await db.query(beforeInsert, { replacements: { email: req.body.email }, type: db.QueryTypes.SELECT });
+    let beforeInsert = 'SELECT COUNT(1) as count FROM user WHERE uid = :uid';
+    let result = await db.query(beforeInsert, { replacements: { uid: req.body.uid }, type: db.QueryTypes.SELECT });
     if (result[0].count == 0) {// login not yet
         let sql = common.buildInsertSql(req, 'user');
         db.query(sql, { replacements: { ...req.body } })
@@ -20,22 +20,22 @@ module.exports.insertNewUser = async (req, res) => {
                         id: result[0],
                         ...req.body
                     },
-                    pd_token: jwt.sign({ userId: result[0] }, process.env.JWT_KEY),
+                    pd_token: jwt.sign({ userId: req.body.uid }, process.env.JWT_KEY),
                     message: 'login successfully!'
                 })
             }).catch(error => {
                 res.status(422).send(error)
             });
     } else {
-        let sql = 'SELECT * FROM user WHERE email = :email';
-        db.query(sql, { replacements: { email: req.body.email }, type: db.QueryTypes.SELECT })
+        let sql = 'SELECT * FROM user WHERE uid = :uid';
+        db.query(sql, { replacements: { uid: req.body.uid }, type: db.QueryTypes.SELECT })
             .then(user => {
                 res.json({
                     result: 'ok',
                     data: {
                         ...user[0]
                     },
-                    pd_token: jwt.sign({ userId: user[0].id }, process.env.JWT_KEY),
+                    pd_token: jwt.sign({ userId: user[0].uid }, process.env.JWT_KEY),
                     message: 'login successfully!'
                 })
             })
